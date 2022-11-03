67784bf276088bd35c899b8d7476160253552e1e
ffaure32 <frfaure32@gmail.com>
2022-11-03 11:02:19 +0100

    approval tests

---
+++ GildedRoseKata/src/test/java/com/gildedrose/GildedRoseApprovalTest.java
@@ -0,0 +1,28 @@
+package com.gildedrose;
+
+import org.approvaltests.combinations.CombinationApprovals;
+import org.approvaltests.legacycode.Range;
+import org.approvaltests.reporters.JunitReporter;
+import org.approvaltests.reporters.UseReporter;
+import org.approvaltests.reporters.intellij.IntelliJReporter;
+import org.junit.jupiter.api.Test;
+
+@UseReporter(IntelliJReporter.class)
+public class GildedRoseApprovalTest {
+    @Test
+    void testAllCombinations() throws Exception {
+
+        String[] items = { "Foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert",
+                "Sulfuras, Hand of Ragnaros", "Conjured" };
+        Integer[] sellins = Range.get(-1, 100);
+        Integer[] qualities = Range.get(-1, 100);
+        CombinationApprovals.verifyAllCombinations(this::checkItem, items, sellins, qualities);
+    }
+
+    public Item checkItem(String item, Integer sellin, Integer quality) {
+        Item[] items = new Item[] { new Item(item, sellin, quality) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        return items[0];
+    }
+}
